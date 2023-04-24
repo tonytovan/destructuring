a6a682e0f19d2dac7f6a40ae9247e6408361f372
tonytovan <tonytovan@gmail.com>
2023-04-24 14:34:11 +0200

    finished

--- destructuring.js
+++ destructuring.js
@@ -5,6 +5,10 @@ const product = {
   refreshRate: 50,
   screenType: 'LED',
 }
+const { price, refreshRate } = product;
+
+console.log(price, refreshRate);
+
 
 // Opdracht 2: Destructure type en brand uit onderstaand object, en log ze daarna in de terminal
 
@@ -17,6 +21,10 @@ const productInformation = {
   screenQuality: 'Ultra HD/4K',
   smartTv: true,
 }
+
+const { type, brand } = productInformation.general;
+
+console.log(type, brand);
 
 // Opdracht 3: destructure wifi en bluetooth uit onderstaand object, en log ze daarna in de terminal
 
@@ -38,14 +46,26 @@ const tvOptions = {
   },
 };
 
+const { wifi, bluetooth } = tvOptions.options.connectivity.popular;
+
+console.log(wifi, bluetooth);
+
 // Opdracht 4: Destructure beide waardes uit onderstaande array, en log ze daarna in de terminal
 
 const connectivity = ['wifi', 'bluetooth'];
 
+const [one, two] = connectivity;
+
+console.log(one, two);
 
 // Opdracht 5: Destructure de >eerste< en >derde< waarde uit onderstaande array, en log ze daarna in de terminal
 
 const tvSizes = [41, 43, 46, 56, 65];
+
+const [first, second, three, four, five] = tvSizes;
+
+console.log(first, three);
+
 
 
 // opdracht 6: Destructure name en adress uit de return value van deze functie en log ze daarna in de terminal
@@ -57,8 +77,14 @@ function getCompanyDetails() {
   }
 }
 
+const { name, adress} = getCompanyDetails();
+console.log(name, adress);
+
 // opdracht 7: Destructure beide zinnetjes uit de return value van de getDetails functie en log ze in de terminal. Let op: de functie verwacht argumenten bij het aanroepen!
 
 function getDetails(name, age) {
   return [`Your name is ${name}`, `You are ${age} years old`];
 }
+
+const [nameString, ageString] = getDetails('Tony', 40);
+console.log(nameString, ageString);
